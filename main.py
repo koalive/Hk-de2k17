1222f93a9db2b5ef855fd8df106f800c76b853f2
Vulliard Loan <lv3895@neuf.fr>
2017-02-23 19:29:46 +0100

    Added S, LD and K matrices

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ Options:
 """
 ################################### Imports ###################################
 from docopt import docopt
+import numpy as np
 
 ################################### Objects ###################################
 
@@ -21,4 +22,27 @@ from docopt import docopt
 arguments = docopt(__doc__)
 
 input_file = open(arguments["<file>"], "r").read().splitlines()
+
+# V = nb of videos
+# E = nb of endpoints
+# R = nb of requests
+# C = nb of cache servers
+# X = capacity of each server in Mb
 V, E, R, C, X = [int(i) for i in input_file[0].split()]
+
+# Size of each video in Mb
+S = np.array([int(i) for i in input_file[1].split()])
+
+# Endpoint description
+parser = 2
+LD = np.empty(E, dtype='int16')
+K = np.empty(E, dtype='int16')
+connectionsDicts = [{} for i in range(E)]
+for i in range(E):
+	LD[i], K[i] = [int(i) for i in input_file[parser].split()]
+	assert LD[i] <= 4000 and LD[i] >= 2
+	parser += 1
+	for j in range(K[i]):
+		c, LC = [int(i) for i in input_file[parser].split()]
+		connectionsDicts[i][c] = LC
+		parser += 1*
